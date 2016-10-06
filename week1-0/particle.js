b3793d381c61add62258e854b654235d123442e8
nahbee <nahbee10@gmail.com>
2016-10-06 14:21:28 +0900

    att repel

---
+++ week1-0/particle.js
@@ -0,0 +1,27 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Child class constructor
+
+// edited by NAHEE KIM
+function Particle(position, radius, range, strength) {
+  VerletParticle2D.call(this,position);
+  this.r = radius;
+  physics.addParticle(this);
+  physics.addBehavior(new AttractionBehavior(this, range, strength));
+
+  // Override the display method
+  this.display = function(){
+    fill(127);
+    stroke(200);
+    strokeWeight(2);
+    //ellipse(this.x,this.y,this.r*2,this.r*2);
+    ellipse(this.x,this.y,20,20);
+  }
+}
+
+// Inherit from the parent class
+Particle.prototype = Object.create(VerletParticle2D.prototype);
+Particle.prototype.constructor = Particle;
+
---
+++ week1-0/sketch.js
@@ -0,0 +1,58 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Reference to physics world
+
+// edited by NAHEE KIM
+var physics;
+
+var particles = [];
+var attractor;
+
+var w = 640;
+var h = 360;
+
+function setup() {
+  createCanvas(w-40,h-40);
+
+  physics=new VerletPhysics2D();
+  physics.setDrag (0.01);
+
+
+  for (var i = 0; i < w/40; i++){
+    for (var j = 0; j < h/40; j++) {
+      particles.push(new Particle(new Vec2D(i*40,j*40)));
+      particles[particles.length-1].lock();
+    }
+  }
+
+  var pp= 0;
+  for (var x = 0; x < w/40; x++){
+    for (var y = 0; y < h/40; y++) {
+      particles.push(new Particle(new Vec2D(x*40,y*40)));
+      physics.addSpring(new VerletSpring2D(particles[pp], particles[w*h/1600+pp], 0, 0.01));
+      pp++;
+    }
+  }
+
+  attractor = new Particle(new Vec2D(mouseX,mouseY),70,150,-5);
+
+
+}
+
+function draw() {
+
+  // Update the physics world
+  physics.update();
+
+  background(51);
+
+  //attractor.display();
+  for (var i = w*h/1600; i < w*h/800; i++) {
+    particles[i].display();
+  }
+
+  attractor.set(mouseX,mouseY);
+
+}
